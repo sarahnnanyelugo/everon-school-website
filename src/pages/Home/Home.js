376812f89d7dad3cc564f0aaf37ce47e58be96de
Sarah Nnanyelugo <sarahnnanyelugo@gmail.com>
2026-05-23 00:43:43 +0100

    extends home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -235,14 +235,77 @@ export const Home = () => {
               </div>
             </div>
           </div>
+          <div className="col-md-8 offset-md-2 values">
+            {" "}
+            <center>
+              <h1>DIFFERENTLY.</h1>
+            </center>
+            <hr />
+            <div className="d-md-flex value-item">
+              <div className="col-md-3 ">
+                <img src={Primary} alt="Avatar" width="90%" />
+              </div>
+              <div className="col-md-4">
+                <h2>CULTURE OF SCHOLARSHIP</h2>
+              </div>
+              <div className="col-md-4">
+                <p>At Everon,scholarship is not confined to the classroom</p>
+              </div>
+            </div>
+            <hr />
+            <div className="d-md-flex value-item">
+              <div className="col-md-4">
+                <h2>INNOVATIVE CURRICULUM</h2>
+              </div>
+              <div className="col-md-4">
+                <p>At Everon,scholarship is not confined to the classroom</p>
+              </div>{" "}
+              <div className="col-md-3 ">
+                <img src={Primary} alt="Avatar" width="90%" />
+              </div>
+            </div>
+            <hr />
+            <div className="d-md-flex value-item">
+              <div className="col-md-3 ">
+                <img src={Primary} alt="Avatar" width="90%" />
+              </div>
+              <div className="col-md-4">
+                <h2>
+                  GIVING <br /> BACK
+                </h2>
+              </div>
+              <div className="col-md-4">
+                <p>At Everon,scholarship is not confined to the classroom</p>
+              </div>
+            </div>
+            <hr />
+            <div className="d-md-flex value-item">
+              <div className="col-md-4">
+                <h2>HOLISTIC DEVELOPMENT</h2>
+              </div>
+              <div className="col-md-4">
+                <p>At Everon,scholarship is not confined to the classroom</p>
+              </div>{" "}
+              <div className="col-md-3 ">
+                <img src={Primary} alt="Avatar" width="90%" />
+              </div>
+            </div>
+            <hr />
+            <div className="d-md-flex value-item">
+              <div className="col-md-3 ">
+                <img src={Primary} alt="Avatar" width="90%" />
+              </div>
+              <div className="col-md-4">
+                <h2>RESEARCH-LED LEARNING</h2>
+              </div>
+              <div className="col-md-4">
+                <p>At Everon,scholarship is not confined to the classroom</p>
+              </div>
+            </div>
+            <hr />
+          </div>
         </div>
       </div>
-      {/* <div class="container col-md-3">
-        <img src={Primary} alt="Avatar" class="image" />
-        <div class="overlay">
-          <div class="text">Hello World</div>
-        </div>
-      </div> */}
     </>
   );
 };
